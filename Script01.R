3871cdd25a52992b3754cf80c5168d62bbc65627
davidps1991 <davichops12@gmail.com>
2015-05-24 18:49:48 -0500

    actualizacion 2

--- Script01.R
+++ Script01.R
@@ -71,27 +71,48 @@ for(i in 1:ncol(data)){
   vacios[i]<-sum(is.na(data[,i]))
   porcentaje[i]<-vacios[i]/nrow(data)*100
 }
-#porcentaje de datos perdidos de las variables
 porcentaje
 
 # 3. Selecionando sujetos mediante un determinado criterio:
 # 3.1 Seleccione los sujetos con una Edad mayor a 40 años.
 # Utilice la función subset()
 
+criterio_edad<-subset(data,subset=data[,"Edad"]>40)
+nrow(criterio_edad)
+#View(criterio_edad)
+
 # 3.2 Seleccione los sujetos que tienen Vivienda Propia.
 
+criterio_vivienda<-subset(data,subset=data[,"Vivienda"]=="Propia")
+nrow(criterio_vivienda)
+#View(criterio_vivienda)
+
 # 3.3 Seleccione los sujetos que tienen más ($>$) de dos cargas familiatres.
+
+criterio_cargas<-subset(data,subset=data[,"Cargas"]>2)
+nrow(criterio_cargas)
+#View(criterio_cargas)
 
 # 3.4 Seleccione los sujetos con una Deuda superior o igual a 500 dólares
 # y más ($>$) de 8 Dias_Atraso.
 
+criterio_deuda<-subset(data,subset=data[,"Deuda"]>=500)
+nrow(criterio_deuda)
+#View(criterio_deuda)
+
 # 3.5 Seleccione los sujetos con un Score mayor o igual a 900 puntos, una Edad menor
 # o igual a 35 años y con más ($>$) de 3 tarjetas de crédito (Numero_TC).
+
+criterio_varios<-subset(data,subset=data[,"Score"]>=900 & data[,"Edad"]<=35 & data[,"Numero_TC"]>3)
+nrow(criterio_varios)
+#View(criterio_varios)
 
 # 4. Gráficos:
 # 4.1 Realice un histograma de la variable Edad, utilice como color de relleno: red
 
+hist(data[,"Edad"],col="red")
+
 # 4.2 Realice un diagrama de cajas de la variable Edad, utilice como color de relleno: green
 # Utilice la función boxplot()
 
-
+boxplot(data[,"Edad"],main = "Variable Edad",col = "yellow")
